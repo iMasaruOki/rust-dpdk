20e715da201f158d3aa74d5e38575a6c52db1f5e
Masaru OKI <masaru.oki@gmail.com>
2017-09-29 08:41:31 +0900

    Decreazse warnings.

--- examples/dpdk-l2fwd/src/main.rs
+++ examples/dpdk-l2fwd/src/main.rs
@@ -15,12 +15,11 @@ static force_quit: bool = false;
 const MAX_PKT_BURST:u16 = 32;
 
 lazy_static! {
-    static ref ports: Mutex<Vec<u8>> = Mutex::new(vec![]);
+    static ref PORTS: Mutex<Vec<u8>> = Mutex::new(vec![]);
 }
 
 unsafe extern "C" fn l2fwd_main_loop(arg: *mut c_void) -> i32 {
     let lcore_id = dpdk::lcore::id();
-    let nports = ports.lock().unwrap().len();
     let mut pkts: [*mut dpdk::ffi::rte_mbuf; MAX_PKT_BURST as usize];
     let mut buffers: [dpdk::eth::tx_buffer; ffi::RTE_MAX_ETHPORTS as usize];
     let in_port = arg as u8;
@@ -39,14 +38,17 @@ unsafe extern "C" fn l2fwd_main_loop(arg: *mut c_void) -> i32 {
         if nb_rx == 0 {
             continue;
         }
-        for out_port in ports.lock().unwrap().iter() {
+        for out_port in PORTS.lock().unwrap().iter() {
             if *out_port == in_port {
                 continue;
             }
-            let mut buffer = &mut buffers[*out_port as usize];
+            let buffer = &mut buffers[*out_port as usize];
             for i in 0..nb_rx as usize {
                 (*pkts[i]).refcnt_update(1);
                 let sent = buffer.tx(*out_port, 0, pkts[i]);
+                if sent < 1 {
+                    (*pkts[i]).refcnt_set((*pkts[i]).refcnt() - 1);
+                }
             }
             buffer.flush(*out_port, 0);
         }
@@ -57,10 +59,10 @@ unsafe extern "C" fn l2fwd_main_loop(arg: *mut c_void) -> i32 {
     0
 }
 
-static mut pktmbuf_pool: *mut ffi::rte_mempool = 0 as *mut ffi::rte_mempool;
 
 fn main() {
     unsafe {
+        let pktmbuf_pool: *mut ffi::rte_mempool;
         let mut opts = Options::new();
         opts.optopt("p", "", "set port bitmap", "PORT");
 
@@ -87,7 +89,7 @@ fn main() {
                     panic!("Not enough logical core.");
                 }
                 println!("portid {}: lcore {}", n, lc);
-                ports.lock().unwrap().push(n);
+                PORTS.lock().unwrap().push(n);
                 lcores.push(lc);
                 lc = dpdk::lcore::get_next(lc, false, false);
             }
@@ -103,7 +105,7 @@ fn main() {
         assert!(pktmbuf_pool.is_null() == false);
         let mut port_conf: ffi::rte_eth_conf = std::mem::zeroed();
         port_conf.rxmode.set_hw_strip_crc(1);
-        for portid in ports.lock().unwrap().clone() {
+        for portid in PORTS.lock().unwrap().clone() {
             let mut info: ffi::rte_eth_dev_info = std::mem::zeroed();
             ffi::rte_eth_dev_info_get(portid, &mut info as *mut ffi::rte_eth_dev_info);
             let device = dpdk::eth::devices(portid);
@@ -140,7 +142,7 @@ fn main() {
         }
         let callback: ffi::lcore_function_t = Some(l2fwd_main_loop);
         for n in 0..lcores.len() {
-            let callback_arg = ports.lock().unwrap()[n] as *mut c_void;
+            let callback_arg = PORTS.lock().unwrap()[n] as *mut c_void;
             ffi::rte_eal_remote_launch(callback, callback_arg,lcores[n]);
         }
         ffi::rte_eal_mp_wait_lcore();
