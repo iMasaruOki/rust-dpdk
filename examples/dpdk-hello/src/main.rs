070fe359d6416b23b6448aa0397233354a88c41f
Masaru OKI <masaru.oki@gmail.com>
2017-09-29 13:46:53 +0900

    Implement launch fns.

--- examples/dpdk-hello/src/main.rs
+++ examples/dpdk-hello/src/main.rs
@@ -1,5 +1,5 @@
 extern crate dpdk;
-use dpdk::ffi;
+use std::os::raw::c_void;
 
 unsafe extern "C" fn hello_thread(_arg: *mut std::os::raw::c_void) -> i32 {
     println!("Hello! lcore {}",  dpdk::lcore::id());
@@ -9,11 +9,8 @@ unsafe extern "C" fn hello_thread(_arg: *mut std::os::raw::c_void) -> i32 {
 fn main() {
     unsafe {
         let _ = dpdk::eal::init(std::env::args());
-        let callback: ffi::lcore_function_t = Some(hello_thread);
-        let callback_arg: *mut std::os::raw::c_void = std::mem::zeroed();
-        ffi::rte_eal_mp_remote_launch(callback,
-                                      callback_arg,
-                                      ffi::rte_rmt_call_master_t::CALL_MASTER);
-        ffi::rte_eal_mp_wait_lcore();
+        let callback_arg: *mut c_void = std::mem::zeroed();
+        dpdk::eal::mp_remote_launch(hello_thread, callback_arg, true);
+        dpdk::eal::mp_wait_lcore();
     }
 }
