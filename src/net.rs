46f6be81dd547f12177b484c9a3d327c86d01581
Masaru OKI <m-oki@lagopus.org>
2017-10-18 12:06:40 +0900

    Add rte_net.

---
+++ src/net.rs
@@ -0,0 +1,9 @@
+// from rte_net.h
+use ffi;
+
+pub unsafe fn get_ptype(m: &ffi::rte_mbuf,
+                        hdr_lens: &mut ffi::rte_net_hdr_lens, layers: u32)
+    -> u32 {
+    ffi::rte_net_get_ptype(m as *const ffi::rte_mbuf,
+                           hdr_lens as *mut ffi::rte_net_hdr_lens, layers)
+}
