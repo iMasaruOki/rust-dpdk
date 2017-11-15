622987b55620d37f6c87d43b09b965ff9169eb79
Masaru OKI <masaru.oki@gmail.com>
2017-11-16 06:35:53 +0900

    Welcome 0.2.0, with DPDK 17.11.
    - port id as u16 in DPDK.
    - struct dpdk::eth::Port is supplied.

--- src/eth.rs
+++ src/eth.rs
@@ -2,11 +2,16 @@
 use ffi;
 use std::ffi::CString;
 
-pub type tx_buffer = ffi::rte_eth_dev_tx_buffer;
-
-pub unsafe fn devices(port_id: u8) -> &'static ffi::rte_eth_dev {
-    &*ffi::rte_eth_devices.as_ptr().offset(port_id as isize)
+#[derive(Copy)]
+pub struct Port {
+    pub port_id: u16,
 }
+
+impl Clone for Port {
+    fn clone(&self) -> Port { *self }
+}
+
+pub type tx_buffer = ffi::rte_eth_dev_tx_buffer;
 
 impl ffi::rte_eth_dev {
     pub unsafe fn is_intr_lsc_enable(&self) -> bool {
@@ -19,89 +24,94 @@ impl ffi::rte_eth_dev {
     }
 }
 
-impl ffi::rte_eth_dev_info {
-    pub unsafe fn get(&mut self, port_id: u8) {
-        ffi::rte_eth_dev_info_get(port_id, self as *mut ffi::rte_eth_dev_info);
+impl Port {
+    pub unsafe fn devices(&self) -> &'static ffi::rte_eth_dev {
+        &*ffi::rte_eth_devices.as_ptr().offset(self.port_id as isize)
     }
-}
 
-pub unsafe fn configure(port_id: u8, nb_rxd: u16, nb_txd: u16,
-                        port_conf: &ffi::rte_eth_conf) -> i32 {
-    ffi::rte_eth_dev_configure(port_id, nb_rxd, nb_txd,
-                               port_conf as *const ffi::rte_eth_conf)
-}
-
-pub unsafe fn socket_id(port_id: u8) -> u32 {
-    ffi::rte_eth_dev_socket_id(port_id) as u32
-}
-
-pub unsafe fn adjust_rx_desc(port_id: u8, nb_rxd: u16) -> u16 {
-    let mut nbr = nb_rxd;
-    let mut nbt = nb_rxd;
-    let rv = ffi::rte_eth_dev_adjust_nb_rx_tx_desc(port_id,
-                                                   &mut nbr, &mut nbt);
-    assert_eq!(rv, 0);
-    nbr
-}
-
-pub unsafe fn adjust_tx_desc(port_id: u8, nb_txd: u16) -> u16 {
-    let mut nbr = nb_txd;
-    let mut nbt = nb_txd;
-    let rv = ffi::rte_eth_dev_adjust_nb_rx_tx_desc(port_id,
-                                                   &mut nbr, &mut nbt);
-    assert_eq!(rv, 0);
-    nbt
-}
-
-pub unsafe fn rx_queue_setup(port_id: u8, queue_id: u16, nb_rxd: u16,
-                             socket_id: u32, rxconf: *mut ffi::rte_eth_rxconf,
-                             pool: *mut ffi::rte_mempool)
-                             -> i32 {
-    ffi::rte_eth_rx_queue_setup(port_id, queue_id, nb_rxd, socket_id,
-                                rxconf, pool)
-}
-
-pub unsafe fn tx_queue_setup(port_id: u8, queue_id: u16, nb_txd: u16,
-                             socket_id: u32, txconf: *mut ffi::rte_eth_txconf)
-                             -> i32 {
-    ffi::rte_eth_tx_queue_setup(port_id, queue_id, nb_txd, socket_id, txconf)
-}
-
-pub unsafe fn start(port_id: u8) -> i32 {
-    ffi::rte_eth_dev_start(port_id)
-}
-
-pub unsafe fn stop(port_id: u8) {
-    ffi::rte_eth_dev_stop(port_id);
-}
-
-pub unsafe fn promiscuous_set(port_id: u8, onoff: bool) {
-    if onoff == true {
-        ffi::rte_eth_promiscuous_enable(port_id);
-    } else {
-        ffi::rte_eth_promiscuous_disable(port_id);
+    pub unsafe fn info(&self, info: &mut ffi::rte_eth_dev_info) {
+        ffi::rte_eth_dev_info_get(self.port_id,
+                                  info as *mut ffi::rte_eth_dev_info);
     }
-}
 
-pub unsafe fn rx_burst(port_id: u8, queue_id: u16,
-                       rx_pkts: *mut &mut ffi::rte_mbuf,
-                       nb_pkts: u16) -> i16 {
-    let dev = devices(port_id);
-    let queue = *(*dev.data).rx_queues.offset(queue_id as isize)
-        as *mut ::std::os::raw::c_void;
-    let nb_rx = (dev.rx_pkt_burst.unwrap())(queue,
-                                            rx_pkts as *mut *mut ffi::rte_mbuf,
-                                            nb_pkts);
-    nb_rx as i16
-}
+    pub unsafe fn configure(&self, nb_rxd: u16, nb_txd: u16,
+                            port_conf: &ffi::rte_eth_conf) -> i32 {
+        ffi::rte_eth_dev_configure(self.port_id, nb_rxd, nb_txd,
+                                   port_conf as *const ffi::rte_eth_conf)
+    }
 
-pub unsafe fn tx_burst(port_id: u8, queue_id: u16,
-                       tx_pkts: *mut *mut ffi::rte_mbuf,
-                       nb_pkts: u16) -> u16 {
-    let dev = devices(port_id);
-    let queue = *(*dev.data).tx_queues.offset(queue_id as isize)
-        as *mut ::std::os::raw::c_void;
-    (dev.tx_pkt_burst.unwrap())(queue, tx_pkts, nb_pkts)
+    pub unsafe fn socket_id(&self) -> u32 {
+        ffi::rte_eth_dev_socket_id(self.port_id) as u32
+    }
+
+    pub unsafe fn adjust_rx_desc(&self, nb_rxd: u16) -> u16 {
+        let mut nbr = nb_rxd;
+        let mut nbt = nb_rxd;
+        let rv = ffi::rte_eth_dev_adjust_nb_rx_tx_desc(self.port_id,
+                                                       &mut nbr, &mut nbt);
+        assert_eq!(rv, 0);
+        nbr
+    }
+
+    pub unsafe fn adjust_tx_desc(&self, nb_txd: u16) -> u16 {
+        let mut nbr = nb_txd;
+        let mut nbt = nb_txd;
+        let rv = ffi::rte_eth_dev_adjust_nb_rx_tx_desc(self.port_id,
+                                                       &mut nbr, &mut nbt);
+        assert_eq!(rv, 0);
+        nbt
+    }
+
+    pub unsafe fn start(&self) -> i32 {
+        ffi::rte_eth_dev_start(self.port_id)
+    }
+
+    pub unsafe fn stop(&self) {
+        ffi::rte_eth_dev_stop(self.port_id);
+    }
+
+    pub unsafe fn promiscuous_set(&self, onoff: bool) {
+        if onoff == true {
+            ffi::rte_eth_promiscuous_enable(self.port_id);
+        } else {
+            ffi::rte_eth_promiscuous_disable(self.port_id);
+        }
+    }
+
+    pub unsafe fn rx_queue_setup(&self, queue_id: u16, nb_rxd: u16,
+                                 socket_id: u32, rxconf: *mut ffi::rte_eth_rxconf,
+                                 pool: *mut ffi::rte_mempool)
+                                 -> i32 {
+        ffi::rte_eth_rx_queue_setup(self.port_id, queue_id, nb_rxd, socket_id,
+                                    rxconf, pool)
+    }
+
+    pub unsafe fn tx_queue_setup(&self, queue_id: u16, nb_txd: u16,
+                                 socket_id: u32, txconf: *mut ffi::rte_eth_txconf)
+                                 -> i32 {
+        ffi::rte_eth_tx_queue_setup(self.port_id, queue_id, nb_txd, socket_id, txconf)
+    }
+
+    pub unsafe fn rx_burst(&self, queue_id: u16,
+                           rx_pkts: *mut &mut ffi::rte_mbuf,
+                           nb_pkts: u16) -> i16 {
+        let dev = self.devices();
+        let queue = *(*dev.data).rx_queues.offset(queue_id as isize)
+            as *mut ::std::os::raw::c_void;
+        let nb_rx = (dev.rx_pkt_burst.unwrap())(queue,
+                                                rx_pkts as *mut *mut ffi::rte_mbuf,
+                                                nb_pkts);
+        nb_rx as i16
+    }
+
+    pub unsafe fn tx_burst(&self, queue_id: u16,
+                           tx_pkts: *mut *mut ffi::rte_mbuf,
+                           nb_pkts: u16) -> u16 {
+        let dev = self.devices();
+        let queue = *(*dev.data).tx_queues.offset(queue_id as isize)
+            as *mut ::std::os::raw::c_void;
+        (dev.tx_pkt_burst.unwrap())(queue, tx_pkts, nb_pkts)
+    }
 }
 
 impl tx_buffer {
@@ -109,24 +119,24 @@ impl tx_buffer {
         ffi::rte_eth_tx_buffer_init(self as *mut tx_buffer, size);
     }
 
-    pub unsafe fn flush(&mut self, port_id: u8, queue_id: u16) -> u16 {
+    pub unsafe fn flush(&mut self, port: Port, queue_id: u16) -> u16 {
         let to_send = self.length;
         if to_send == 0 {
             return 0;
         }
-        let sent = tx_burst(port_id, queue_id,
-                            self.pkts.as_mut_ptr(), to_send);
+        let sent = port.tx_burst(queue_id,
+                                 self.pkts.as_mut_ptr(), to_send);
         self.length = 0;
         sent
     }
 
-    pub unsafe fn tx(&mut self, port_id: u8, queue_id: u16,
+    pub unsafe fn tx(&mut self, port: Port, queue_id: u16,
                      tx_pkt: *mut ffi::rte_mbuf) -> u16 {
         *self.pkts.as_mut_ptr().offset(self.length as isize) = tx_pkt;
         self.length += 1;
         if self.length < self.size {
             return 0;
         }
-        self.flush(port_id, queue_id)
+        self.flush(port, queue_id)
     }
 }
